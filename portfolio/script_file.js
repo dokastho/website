b15228097df7953be305a7761df103469e74b5d9
Thomas Dokas <dokastho@umich.edu>
2021-09-07 14:24:52 -0400

    fixing some directory problems

--- portfolio/script_file.js
+++ portfolio/script_file.js
@@ -1,14 +1,18 @@
-// Starter Code courtesy of Kindacode.com
-// https://www.kindacode.com/article/how-to-get-all-links-from-a-webpage-using-node-js/
-const got = require('got');
-const cheerio = require('cheerio');
-
-const url = 'http://github.com/dokastho';
-
-const extractLinks = async (res) => {
-  try {
-    // Fetching HTML
-    const response = await got(url);
+// written by thomas dokas, dokastho@umich.edu
+const extractLinks = async () => {
+  // Fetching HTML
+  const request = require('request');
+  let options = {
+    url: url,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+    }
+  };
+  request(options, function(err, resp, html) {
+    // fix indent
+    if (err) {
+      console.log(err.response.body);
+    }
     const html = response.body;
 
     // Using cheerio to extract <a> tags
@@ -32,23 +36,29 @@ const extractLinks = async (res) => {
           links.push(repoLink);
         }
       
-    });
+      });
 
-    // console.log(links);
-    // going to want to load index.html down below and update it here using cheerio.
-    // simply update the document's div with the snapshots/ projects, and add the
-    // sidebar either via a seperate function or inline
-    res.end(links.toString().concat('\n'));
-  } catch (error) {
-    console.log(error.response.body);
-  }
+      // console.log(links);
+      // going to want to load index.html down below and update it here using cheerio.
+      // simply update the document's div with the snapshots/ projects, and add the
+      // sidebar either via a seperate function or inline
+      // res.end(links.toString().concat('\n'));
+      $('h2.title').text('tester hello');
+      $('h2').addClass('tester welcome');
+      links.forEach((elt) => {
+        $('h2').text(elt);
+      });
+      $.html();
+    });
 };
 
+
+const cheerio = require('cheerio');
 const http = require('http');
-const fs = require('fs');
+const url = 'http://github.com/dokastho';
 
 http.createServer(function (req,res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    extractLinks(res);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  extractLinks();
 }).listen(8088,'localhost');
 console.log('Server running at http://localhost:8088');
